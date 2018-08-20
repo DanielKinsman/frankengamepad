7d97c409cb46a7a633c6821a0c332ac3c1b0dd58
Daniel Kinsman <danielkinsman@riseup.net>
2018-08-20 21:30:23 +1000

    fix logging and better cli ux

--- frankengamepad/__init__.py
+++ frankengamepad/__init__.py
@@ -1,3 +1,24 @@
 """
 TODO description
 """
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+def setup_logging(logfile):
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(levelname)s:%(name)s:%(asctime)s:%(message)s")
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+
+    if logfile is not None:
+        file_handler = logging.FileHandler(logfile)
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+        logger.info(f"Writing log to {logfile}")
--- frankengamepad/main.py
+++ frankengamepad/main.py
@@ -1,36 +1,31 @@
 import click
 import logging
 import sys
+from . import setup_logging
 from . import config as fconfig
 from . import device as fdevice
+
 
 logger = logging.getLogger(__name__)
 
 
-def setup_logging(logfile):
-    logger.setLevel(logging.DEBUG)
-    formatter = logging.Formatter("%(levelname)s:%(name)s:%(asctime)s:%(message)s")
-
-    stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(logging.INFO)
-    stream_handler.setFormatter(formatter)
-    logger.addHandler(stream_handler)
-
-    if logfile is not None:
-        file_handler = logging.FileHandler(logfile)
-        file_handler.setLevel(logging.DEBUG)
-        file_handler.setFormatter(formatter)
-        logger.addHandler(file_handler)
-        logger.info(f"Writing log to {logfile}")
-
-
 @click.command()
-@click.argument("config_file", default=fconfig.default_config_file(),
-    type=click.Path(exists=True, dir_okay=False, resolve_path=True))
-@click.option("--logfile", default=None, type=click.Path(dir_okay=False, resolve_path=True))
-@click.option("--interactive", is_flag=True)
-def main(config_file, logfile, interactive):
-    setup_logging(logfile)
+@click.option("--config-file", default=None,
+    type=click.Path(exists=True, dir_okay=False, resolve_path=True),
+    help="A yaml configuration file describing gamepad mappings.")
+@click.option("--log-file", default=None,
+        type=click.Path(dir_okay=False, resolve_path=True),
+        help="File to store the log in.")
+@click.option("--interactive", is_flag=True,
+        help="Creates virtual gamepads then launches an interactive "
+        "python prompt before beginning to map input devices to output "
+        "devices. Useful for games you need to start with only the "
+        "virtual gamepad before doing anything else.")
+def main(config_file, log_file, interactive):
+    setup_logging(log_file)
+
+    if config_file is None:
+        config_file=fconfig.default_config_file()
 
     logger.info(f"loading config from `{config_file}`")
     config = fconfig.load(config_file)

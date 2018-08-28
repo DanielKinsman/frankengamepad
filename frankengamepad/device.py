612fc18e9d9a2a01b1984c27212e11035155a601
Daniel Kinsman <danielkinsman@riseup.net>
2018-08-28 22:47:11 +1000

    monitor devices as they come off/online

--- frankengamepad/device.py
+++ frankengamepad/device.py
@@ -1,14 +1,82 @@
 import asyncio
 import evdev
 import logging
+import signal
 import time
 import frankengamepad.config
 
 
+VENDOR = 0x3748  # randomly chosen
+PRODUCT = 0xf366  # randomly chosen
+
 logger = logging.getLogger(__name__)
 
 
+class NoDeviceError(Exception):
+    pass
+
+
+def get_device(config):
+    """
+    Get the device from the source configuration.
+    :param config: The "source" device configuration containing a "name"
+                   (e.g. "Xbox 360 Wireless Receiver (XBOX)") and/or a
+                   "path" (e.g. "/dev/input/event19")
+    :type config: collections.Mapping
+    :return: the input device
+    :rtype: evdev.InputDevice
+    """
+    try:
+        device = evdev.InputDevice(config["path"])
+        try:
+            if device.name == config["name"]:
+                return device
+        except KeyError:
+            return device  # No name specified in config
+    except(KeyError, FileNotFoundError):
+        pass
+
+    for path in evdev.list_devices():
+        device = evdev.InputDevice(path)
+        if device.name == config["name"]:
+            return device
+
+    raise NoDeviceError(f"No device found with name `{config.get('name')}` path `{config.get('path')}`")
+
+
+async def watch_device(config, franken_uinputs):
+    """
+    Watch the device specified in the source configuration and translate events
+    from the source device to a franken gamepad.
+    :param config: The device configuration (from the yaml "sources" configuration section)
+    :type config: collections.Mapping
+    :param franken_uinputs: a mapping from device name to the devices uinput object
+    :type uinputs: collections.Mapping {device_name: evdev.UInput}
+    """
+    while(True):
+        try:
+            device = get_device(config)
+            if config.get("exclusive", False):
+                with device.grab_context():
+                    await asyncio.ensure_future(process_events(device, config["events"], franken_uinputs))
+            else:
+                await asyncio.ensure_future(process_events(device, config["events"], franken_uinputs))
+        except NoDeviceError as e:
+            logger.error(str(e))
+            await asyncio.sleep(10)
+
+
 async def process_events(device, config, franken_uinputs):
+    """
+    Translate events from a source input device to a target uinput device.
+    :param device: the source input device
+    :type device: evdev.InputDevice
+    :param config: the configuration for the source device (from the yaml)
+    :type config: collections.Mapping
+    :param franken_uinputs: the uinput device to send events to
+    :type franken_uinputs: collections.Mapping {device_name: evdev.UInput}
+    """
+    logger.info(f"Processing events from {device.path} {device.name}")
     hooked_uinputs = []
     for event_code_config in config.values():
         for franken_device_name in event_code_config.keys():
@@ -21,6 +89,7 @@ async def process_events(device, config, franken_uinputs):
     }
 
     async for event in device.async_read_loop():
+        # TODO raise a NoDeviceError if the device goes away
         # always pass on sync events
         if event.type == evdev.ecodes.EV_SYN:
             for franken_uinput in hooked_uinputs:
@@ -85,9 +154,15 @@ def franken_value(original_value, original_absinfo, franken_absinfo):
 
 def button(uinput, code, value=1, syn=True):
     """
-    :param franken_uinput: the evdev.UInput device to generate the button event
+    Send a button event.
+    :param uinput: the uinput device to generate the button event
+    :type uinput: evdev.UInput
     :param code: the event code, e.g. evdev.ecodes.BTN_A
+    :type code: int
     :param value: 1 (button down), 0 (button up)
+    :type code: int
+    :param syn: whether or not to follow up the event with a syn event
+    :type syn: bool
     """
     # sec and usec are thrown out anyway, see
     # https://github.com/gvalkov/python-evdev/blob/master/evdev/eventio.py#L111
@@ -97,6 +172,13 @@ def button(uinput, code, value=1, syn=True):
 
 
 def button_toggle(uinput, code):
+    """
+    Send a button down event, followed a short time later by a button up event.
+    :param uinput: the uinput device to generate the button event
+    :type uinput: evdev.UInput
+    :param code: the event code, e.g. evdev.ecodes.BTN_A
+    :type code: int
+    """
     button(uinput, code, value=1)
     time.sleep(0.1)
     button(uinput, code, value=0)
@@ -112,7 +194,7 @@ def make_franken_uinputs(config):
 
         capabilities = {k: v for k, v in capabilities.items()
                 if k not in {evdev.ecodes.EV_SYN, evdev.ecodes.EV_FF}}
-        return evdev.UInput(capabilities, name=name)
+        return evdev.UInput(capabilities, name=name, vendor=VENDOR, product=PRODUCT)
 
     frankens = {k: make_uinput(k, v) for k, v in config.items()}
     for name, uinput in frankens.items():
@@ -122,27 +204,17 @@ def make_franken_uinputs(config):
 
 
 def run_translations(config, franken_uinputs):
-    all_devices = (evdev.InputDevice(p) for p in evdev.list_devices())
-    all_devices = {d.name: d for d in all_devices}
-
-    grabbed = []
-    try:
-        for source in config.values():
-            try:
-                device  = all_devices[source["name"]]
-            except KeyError:
-                raise KeyError(f"Could not find device `{source['name']}`")
-
-            if source.get("exclusive", False):
-                device.grab()
-                grabbed.append(device)
-                logger.info(f"Grabbed exclusive access to {device.path}")
-
-            asyncio.ensure_future(process_events(device, source["events"], franken_uinputs))
-
-        loop = asyncio.get_event_loop()
-        loop.run_forever()
-    finally:
-        for d in grabbed:
-            d.ungrab()
-            logger.info(f"Released exclusive access to {d.path}")
+    """
+    Using the frankegamepad configuration (from the yaml), watch devices, listen
+    to their events, and translate them to the franken gamepads.
+    This method will block forever until SIGINT or SIGTERM is received.
+    :param config: The sources configuration from the yaml
+    :type config: collections.Mapping
+    :param franken_uinputs: A mapping of uinput devices for franken gamepads
+    :type franken_uinputs: collections.Mapping {device_name: evdev.UInput}
+    """
+    loop = asyncio.get_event_loop()
+    loop.add_signal_handler(signal.SIGINT, loop.stop)
+    loop.add_signal_handler(signal.SIGTERM, loop.stop)
+    asyncio.gather(*(watch_device(source, franken_uinputs) for source in config.values()))
+    loop.run_forever()
